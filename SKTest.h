7306c2dd0c91f55e44bd81ed8b83eb47bd139107
Scott Kolp <scottkolp@Scotts-MacBook-Pro.local>
2014-02-20 23:58:55 -0500

    added test class

---
+++ SKTest.h
@@ -0,0 +1,13 @@
+//
+//  SKTest.h
+//  Git Test
+//
+//  Created by Scott Kolp on 2/20/14.
+//  Copyright (c) 2014 Scott Kolp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKTest : NSObject
+
+@end
---
+++ SKTest.m
@@ -0,0 +1,13 @@
+//
+//  SKTest.m
+//  Git Test
+//
+//  Created by Scott Kolp on 2/20/14.
+//  Copyright (c) 2014 Scott Kolp. All rights reserved.
+//
+
+#import "SKTest.h"
+
+@implementation SKTest
+
+@end
